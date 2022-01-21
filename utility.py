54c9dd139ecee3168eb8593c4c75bdf425a5b34e
Abdelrhman Bassiouny <Abdelrhman_bassuny@hotmail.com>
2022-01-21 10:01:30 +0100

    don't plot loss

--- utility.py
+++ utility.py
@@ -88,7 +88,7 @@ class checkpoint():
     def save(self, trainer, epoch, is_best=False):
         trainer.model.save(self.get_path('model'), epoch, is_best=is_best)
         trainer.loss.save(self.dir)
-        trainer.loss.plot_loss(self.dir, epoch)
+        # trainer.loss.plot_loss(self.dir, epoch)
 
         self.plot_psnr(epoch)
         trainer.optimizer.save(self.dir)
