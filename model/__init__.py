3aa014e3367964f9368a94310e71570a010281c7
Abdelrhman Bassiouny <Abdelrhman_bassuny@hotmail.com>
2022-01-21 23:16:44 +0100

    cont.debugging

--- model/__init__.py
+++ model/__init__.py
@@ -419,7 +419,7 @@ class Model(nn.Module):
         print("\n\n\n y_h_cut_shape ===== ", y_h_cut.shape)
         y_h_cut_unfold = y_h_cut_unfold[...,:,int(shave/2*scale):padsize*scale-int(shave/2*scale)].contiguous()
         print("\n\n\n y_h_cut_unfold_shape ===== ", y_h_cut_unfold.shape)
-        y_h_cut_inter = torch.nn.functional.fold(input_y,(padsize*scale,(w-w_cut-shave)*scale), (padsize*scale,padsize*scale-shave*scale), stride=int(shave/2*scale))
+        y_h_cut_inter = torch.nn.functional.fold(y_h_cut_unfold,(padsize*scale,(w-w_cut-shave)*scale), (padsize*scale,padsize*scale-shave*scale), stride=int(shave/2*scale))
         
         y_ones = torch.ones(y_h_cut_inter.shape, dtype=y_h_cut_inter.dtype)
         print("\n\n\n y_h_cut_inter_shape ===== ", y_h_cut_inter.shape)
